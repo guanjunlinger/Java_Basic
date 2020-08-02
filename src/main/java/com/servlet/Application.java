8020081eeb138a931278bad464f30a72d9a7ba84
guanjun <guanjun@itiger.com>
2020-08-02 14:26:36 +0800

    Thread 线程控制

--- src/main/java/com/servlet/Application.java
+++ src/main/java/com/servlet/Application.java
@@ -2,12 +2,11 @@ package com.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
